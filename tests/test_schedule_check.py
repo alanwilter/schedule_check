a8fa6f4203158b552dcd5290ba645f605f9735a7
Alan Silva <3899850+alanwilter@users.noreply.github.com>
2022-01-27 07:53:29 +0000

    Test: added CLI and improved comparator

--- tests/test_schedule_check.py
+++ tests/test_schedule_check.py
@@ -1,15 +1,10 @@
 from functools import cmp_to_key
+from subprocess import STDOUT, check_output
 
 import pytest
 
 from schedule_check import __version__
-from schedule_check.report_meetings_clashes import (
-    Meeting,
-    clashing_meetings,
-    get_clashes,
-    get_meetings_list,
-    get_time_obj,
-)
+from schedule_check.report_meetings_clashes import Meeting, clashing_meetings, get_meetings_list, get_time_obj
 
 
 def test_version():
@@ -56,9 +51,13 @@ def test_comparator():
     m1 = Meeting("test1", "7:00", "8:00am")
     m2 = Meeting("test2", "7:00", "7:30")
     m3 = Meeting("test3", "7:30", "8:30am")
-    mm = [m3, m1, m2]
+    m4 = Meeting("test4", "7:30", "9:30am")
+    m5 = Meeting("test0", "7:00", "7:30am")  # clash m2, name diff
+    m6 = Meeting("test3", "7:30", "8:30am")  # m6 = m3
+    m7 = Meeting("test7", "7:00", "8:00am")  # m7 = m1
+    mm = [m3, m1, m2, m4, m5, m6, m7]
     ms = sorted(mm, key=cmp_to_key(Meeting.comparator))
-    assert ms == [m2, m1, m3]
+    assert ms == [m5, m2, m1, m7, m3, m6, m4]
 
 
 def test_clashes():
@@ -71,3 +70,14 @@ def test_clashes():
     m_list.sort(key=cmp_to_key(Meeting.comparator))
     c = clashing_meetings(m_list)
     assert repr(c) == "[(Meeting 1 @ 09:00_10:00, Meeting 2 @ 09:30_10:30, 30)]"
+
+
+def test_cli():
+    msgs = [
+        "Meetings: <Meeting 2 @ 09:00_10:00> and <Meeting 3 @ 09:30_10:30> overlaps for 30 minutes (between 10:00",
+        "Meetings: <Meeting 4 @ 10:45_13:00> and <Meeting 5 @ 12:00_13:00> overlaps for 60 minutes (between 13:00",
+    ]
+    cmd = "env python3 ./schedule_check/report_meetings_clashes.py -i tests/times"
+    out = check_output(cmd, shell=True, stderr=STDOUT).decode()
+    for msg in msgs:
+        assert msg in out
